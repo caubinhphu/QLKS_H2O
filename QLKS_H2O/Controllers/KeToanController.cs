961e5c9e572c4b1f02210c86daca5df1608f524e
Hai tran do <10a3nguyenanhhai@gmail.com>
2020-05-18 01:54:00 +0700

    add layout

---
+++ QLKS_H2O/Controllers/KeToanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLKS_H2O.Controllers
+{
+    public class KeToanController : Controller
+    {
+        // GET: KeToan
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
